38b7bb16b78a1146ee92dc6042a2f959d095c5d6
Indrė <90854409+IndrePet@users.noreply.github.com>
2022-05-26 15:26:31 +0300

    adding basic validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,19 +2,51 @@ import Card from '../UI/Card';
 import Button from '../UI/Button';
 
 import styles from './AddUser.module.css';
+import { useState } from 'react';
 
 const AddUser = () => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+
+    if (+enteredAge < 1) {
+      return;
+    }
+
+    setEnteredAge('');
+    setEnteredUsername('');
   };
 
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age (years)</label>
-        <input id="age" type="text" />
+        <input
+          id="age"
+          type="text"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
