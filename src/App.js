8118a28f29d35d6b678e4cd1e1767d0c5b924da6
Indrė <90854409+IndrePet@users.noreply.github.com>
2022-05-26 14:19:42 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import React, { useState } from 'react';
-import NewUser from './components/NewUser/NewUser';
-import UsersList from './components/Users/UsersList';
+import React from 'react';
 
-import './App.css';
 
 function App() {
-  const [users, setUsers] = useState([]);
-
-  const addUserHandler = (newUser) => {
-    setUsers((prevUserList) => {
-      return [newUser, ...prevUserList];
-    });
-  };
-
   return (
     <div>
-      <NewUser onAddUser={addUserHandler} />
-      <UsersList data={users} />
+
     </div>
   );
 }
