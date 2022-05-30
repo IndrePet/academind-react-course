b528092a48c2df2f403e26b227e63c2bdc808bc7
Indrė <90854409+IndrePet@users.noreply.github.com>
2022-05-30 16:25:26 +0300

    Added cart modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Modal from '../UI/Modal';
 
 import styles from './Cart.module.css';
 
-const Cart = () => {
+const Cart = (props) => {
   const cartItems = (
     <ul className={styles['cart-items']}>
       {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
@@ -12,17 +13,19 @@ const Cart = () => {
   );
 
   return (
-    <div>
+    <Modal onHideCart={props.onHideCart}>
       {cartItems}
       <div className={styles.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={styles.actions}>
-        <button className={styles['button--alt']}>Close</button>
+        <button className={styles['button--alt']} onClick={props.onHideCart}>
+          Close
+        </button>
         <button className={styles.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
