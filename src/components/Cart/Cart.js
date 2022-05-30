002c2e42e74a89a699848e0a4a0cfe2225a07c69
Indrė <90854409+IndrePet@users.noreply.github.com>
2022-05-30 17:51:31 +0300

    Adding cart context

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal';
 
 import styles from './Cart.module.css';
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
   const cartItems = (
     <ul className={styles['cart-items']}>
-      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <li className={styles['cart-items']}>{item.name}</li>
       ))}
     </ul>
   );
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
   return (
     <button className={styles.button} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{cartCtx.items.length}</span>
     </button>
   );
 };
