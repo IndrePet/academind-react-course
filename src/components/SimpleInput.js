97c821747cf8ac1ac332386b5a7dd0572b00cef5
Indrė <90854409+IndrePet@users.noreply.github.com>
2022-06-06 13:03:54 +0300

    managing form validity

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -5,43 +5,58 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 const SimpleInput = (props) => {
-  // const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
 
   let helperText = '';
-
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
+  };
+
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+    if (enteredName.trim() === '') {
+      helperText = 'Please enter a valid name (from submission handler)';
+      console.log(helperText);
+      return;
+    }
   };
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
-    if (enteredName.trim() === '') {
-      setEnteredNameIsValid(true);
+    setEnteredNameTouched(true);
+
+    if (!enteredNameIsValid) {
       helperText = 'Please enter a valid name (from submission handler)';
       console.log(helperText);
       return;
     }
-    setEnteredNameIsValid(false);
-    // const enteredValue = nameInputRef.current.value;
-    console.log(enteredName);
     setEnteredName('');
+    setEnteredNameTouched(false);
   };
 
   return (
-    <Box component="form" onSubmit={formSubmissionHandler}>
+    <Box component="form" noValidate onSubmit={formSubmissionHandler}>
       <TextField
-        // inputRef={nameInputRef}
         required
         id="name"
         label="Your Name"
         fullWidth
         onChange={nameInputChangeHandler}
+        onBlur={nameInputBlurHandler}
         value={enteredName || ''}
-        helperText={enteredNameIsValid && 'Enter valid name (from attribute)'}
-        error={enteredNameIsValid}
+        helperText={nameInputIsInvalid && 'Enter valid name (from attribute)'}
+        error={nameInputIsInvalid}
       />
       <Button
         fullWidth
@@ -49,6 +64,7 @@ const SimpleInput = (props) => {
         sx={{ mt: 3, mb: 2 }}
         type="submit"
         color="success"
+        disabled={!formIsValid}
       >
         Submit
       </Button>
