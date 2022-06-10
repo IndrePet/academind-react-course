91017bb6892860472ee79ca537d76903009da9d8
Indrė <90854409+IndrePet@users.noreply.github.com>
2022-06-10 14:46:02 +0300

    Redux Toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,34 +1,26 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { counterActions } from '../store/counterSlice';
 import classes from './Counter.module.css';
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
-  const showCounter = useSelector((state) => state.showCounter);
+  const counter = useSelector((state) => state.counter.counter);
+  const showCounter = useSelector((state) => state.counter.showCounter);
 
   const incrementHandler = () => {
-    dispatch({
-      type: 'INCREMENT',
-    });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({
-      type: 'INCREASE',
-      amount: 5,
-    });
+    dispatch(counterActions.increase(5));
   };
 
   const decrementHandler = () => {
-    dispatch({
-      type: 'DECREMENT',
-    });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({
-      type: 'TOGGLE',
-    });
+    dispatch(counterActions.toggle());
   };
 
   const card = (
