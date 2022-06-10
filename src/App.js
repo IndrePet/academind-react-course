29d9027d6c5ef08c62eb88dd63ddb3f14ec897a5
Indrė <90854409+IndrePet@users.noreply.github.com>
2022-06-10 15:42:51 +0300

    Base code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import Counter from './components/Counter';
-import Header from './components/Header';
-import Auth from './components/Auth';
-import UserProfile from './components/UserProfile';
-import { useSelector } from 'react-redux';
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
 function App() {
-  const auth = useSelector((state) => state.auth.isAuthenticated);
-
   return (
-    <>
-      <Header />
-      {auth ? <UserProfile /> : <Auth />}
-      <Counter />
-    </>
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
   );
 }
 
