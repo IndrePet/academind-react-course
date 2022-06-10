5551dbfe36f874dea763b4c9e0cf61bc46a51702
Indrė <90854409+IndrePet@users.noreply.github.com>
2022-06-10 13:20:05 +0300

    state management with Redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,51 @@
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
+
+  const incrementHandler = () => {
+    dispatch({
+      type: 'INCREMENT',
+    });
+  };
+
+  const increaseHandler = () => {
+    dispatch({
+      type: 'INCREASE',
+      amount: 5,
+    });
+  };
+
+  const decrementHandler = () => {
+    dispatch({
+      type: 'DECREMENT',
+    });
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch({
+      type: 'TOGGLE',
+    });
+  };
+
+  const card = (
+    <>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+    </>
+  );
 
   return (
     <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {showCounter && card}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
